d6372e5bff6a5363f011668ae8293cc9f1b73329
Tobias Grotheer <saibot@fn.de>
2023-08-08 22:42:09 +0200

    add argument to show version and set to 0.0.2

--- cmd/root.go
+++ cmd/root.go
@@ -4,10 +4,17 @@ Copyright © 2023 Tobias Grotheer <tobias@grotheer-web.de>
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 )
+
+const (
+	version = "0.0.2"
+)
+
+var showVersion bool
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -15,6 +22,7 @@ var rootCmd = &cobra.Command{
 	Short: "A CLI for accessing a rest api.",
 	Long: `With this CLI you can access a rest api. Its main purpose is the check command, to run
 Nagio compatible checks agains a rest api endpoint.`,
+	Run: runRoot,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -27,4 +35,12 @@ func Execute() {
 }
 
 func init() {
+	rootCmd.Flags().BoolVarP(&showVersion, "version", "V", false, "Show the version")
+}
+
+func runRoot(cmd *cobra.Command, args []string) {
+	if showVersion {
+		fmt.Printf("rest-api-cli version %s\n", version)
+	}
+	os.Exit(0)
 }
